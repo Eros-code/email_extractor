d39a80d03a4dcd8a04857ea6221ccf888629ef33
Eros-code <eros.achillea@outlook.com>
2023-11-12 00:55:04 +0000

    added functionality for email extractor to discern if email template is included in most recent email

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from email.header import decode_header
 import webbrowser
 import os
 from dotenv import load_dotenv
+import re
 
 # Load the dotenv file
 load_dotenv()
@@ -23,7 +24,9 @@ def clean(text):
     return "".join(c if c.isalnum() else "_" for c in text)
 
 def retrieveEmailMessages(messages, N):
+    emailList = []
     for i in range(messages, messages-N, -1):
+        emailDictionary = {}
     # fetch the email message by ID
         res, msg = imap.fetch(str(i), "(RFC822)")
         for response in msg:
@@ -40,9 +43,10 @@ def retrieveEmailMessages(messages, N):
                 if isinstance(From, bytes):
                     From = From.decode(encoding)
 
-                print("Subject:", subject)
-                print("From:", From)
-                print("Date", msg['Date'])
+                emailDictionary['Subject'] = subject
+                emailDictionary['From'] = From
+                emailDictionary['Date'] = msg['date']
+
                 # if the email message is multipart
                 if msg.is_multipart():
                     # iterate over email parts
@@ -57,9 +61,10 @@ def retrieveEmailMessages(messages, N):
                             pass
                         if content_type == "text/plain" and "attachment" not in content_disposition:
                             # print text/plain emails and skip attachments
-                            print(body)
+                            emailDictionary['body'] = body
+                            emailDictionary['attachment'] = 'No'
                         elif "attachment" in content_disposition:
-                            print('there are attachments in this email')
+                            emailDictionary['attachment'] = 'Yes'
                 else:
                     # extract content type of email
                     content_type = msg.get_content_type()
@@ -67,13 +72,32 @@ def retrieveEmailMessages(messages, N):
                     body = msg.get_payload(decode=True).decode()
                     if content_type == "text/plain":
                         # print only text email parts
-                        print(body)
+                        emailDictionary['body'] = body
+                        emailDictionary['attachment'] = 'No'
                 if content_type == "text/html":
                     # if it's HTML, create a new HTML file and open it in browser
                     pass
+        emailList.append(emailDictionary)
     # close the connection and logout
     imap.close()
     imap.logout()
+
+    return emailList
+
+def templateChecker(retrievedEmail):
+    matches = ["ACTION:", 
+                   "EMAIL:",
+                   "DASHBOARD_NAME(S):",
+                   "TEAM:",
+                   "SC END DATE:",
+                   "SC STATUS:",
+                   "AUDIT SOURCE:",
+                   "USER_TO_COPY_EMAIL:"]
+    if all(variable in retrievedEmail['body'] for variable in matches):
+        return retrievedEmail['body']
+    else:
+        return 'template not detected'
+    
 
 # We've imported the necessary modules and then specified the credentials of our email account. 
 
@@ -94,6 +118,41 @@ if __name__=='__main__':
         # total number of emails
         messages = int(messages[0])
         # We've used the imap.select() method, which selects a mailbox (Inbox, spam, etc.), we've chosen the INBOX folder. You can use the imap.list() method to see the available mailboxes.
-        retrieveEmailMessages(messages, N)
+        retrievedEmail = retrieveEmailMessages(messages, N)[0]
+        emailOutput = templateChecker(retrievedEmail)
+        if emailOutput != 'template not detected':
+            ActionIndex = emailOutput.find('ACTION:')+7
+            EmailIndex = emailOutput.find('EMAIL:')+6
+            DashboardIndex = emailOutput.find('DASHBOARD_NAME(S):') + 18
+            TeamIndex = emailOutput.find('TEAM:') + 5
+            ScEndIndex = emailOutput.find('SC END DATE:') + 12
+            ScStatusIndex = emailOutput.find('SC STATUS:') + 10
+            AuditSourceIndex = emailOutput.find('AUDIT SOURCE:') + 13
+            UserCopyIndex = emailOutput.find('USER_TO_COPY_EMAIL:') + 19
+            
+            variablesDict = {}
+            variablesDict['ACTION'] = emailOutput[ActionIndex:EmailIndex-6]
+            variablesDict['EMAIL'] = emailOutput[EmailIndex:DashboardIndex-18]
+            variablesDict['DASHBOARD_NAME(S)'] = emailOutput[DashboardIndex:TeamIndex-5]
+            variablesDict['TEAM'] = emailOutput[TeamIndex:ScEndIndex-12]
+            variablesDict['SC_END_DATE'] = emailOutput[ScEndIndex:ScStatusIndex-10]
+            variablesDict['SC_STATUS'] = emailOutput[ScStatusIndex:AuditSourceIndex-13]
+            variablesDict['AUDIT_SOURCE'] = emailOutput[AuditSourceIndex:UserCopyIndex-19]
+            percentIndex = emailOutput[UserCopyIndex:].index('%')
+            UserCopy = emailOutput[UserCopyIndex:]
+            variablesDict['USER_TO_COPY_EMAIL'] = UserCopy[:percentIndex]
+
+            for i in variablesDict.keys():
+                variablesDict[i] = variablesDict[i].replace("\r\n", " ")
+                variablesDict[i] = variablesDict[i].strip()
+
+
+            print(variablesDict)
+
+
+        else:
+            print(emailOutput)
+
+        
     except:
         print('login to outlook email account was not successful')
