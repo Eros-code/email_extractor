6d1258a5f4318849fd0108d9806559491e23e2ba
Hakan Bas <hakan.bas@digital.hmrc.gov.uk>
2023-11-14 11:02:03 +0000

    configuring jira options

--- ticket_creator.py
+++ ticket_creator.py
@@ -1,6 +1,9 @@
 import os 
 from dotenv import load_dotenv
+from jira import JIRA
+
 load_dotenv()
 jira_auth_key = os.getenv('JIRA_AUTH_KEY')
-print('testing')
-
+email = os.getenv('JIRA_EMAIL')
+jira_server = os.getenv('JIRA_SERVER')
+jiraOptions = {'server': jira_server }
