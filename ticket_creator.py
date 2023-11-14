ceb46927955da99e9367b8136d816dc9a6c9d787
Hakan Bas <hakan.bas@digital.hmrc.gov.uk>
2023-11-14 12:04:50 +0000

    added input variables to functions

--- ticket_creator.py
+++ ticket_creator.py
@@ -7,16 +7,28 @@ load_dotenv()
 jira_auth_key = os.getenv('JIRA_AUTH_KEY') #Jira api key
 jira_email = os.getenv('JIRA_EMAIL') #email used for Jira account
 jira_server = os.getenv('JIRA_SERVER') # Jira board URL, looks like ______.atlassian.net
-jiraOptions = {'server': jira_server } #
+jiraOptions = {'server': jira_server } 
+
 
 def connect_to_jira():
     jira = JIRA(options=jiraOptions, basic_auth=(jira_email, jira_auth_key))
     return jira
 
-def create_new_issue():
-    new_issue = jira.create_issue(project='TJIC', summary='New issue from jira-python',
-                                description='Look into this one', issuetype={'id': '10001'})
+def create_new_issue(project_name, issue_summary, issue_description, issue_type = 'Task'):
+    new_issue = jira.create_issue(project = project_name, summary = issue_summary,
+                                description = issue_description, issuetype={'name': issue_type})
     
-def list_all_issues():
-    for singleIssue in jira.search_issues(jql_str='project = TJIC'):
+def list_all_issues(project_name):
+    for singleIssue in jira.search_issues(jql_str = f'project = {project_name}'):
         print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary,singleIssue.fields.reporter.displayName))
+
+
+if __name__=='__main__':
+    jira = connect_to_jira()
+
+    project_name = 'TJIC'
+    issue_summary = 'work hard'
+    issue_description = 'work harder'
+    
+    create_new_issue(project_name, issue_summary, issue_description)
+    list_all_issues(project_name)
