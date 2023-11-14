1fed67477e317ce954f060b30c3f1c8d8153ffa5
Hakan Bas <hakan.bas@digital.hmrc.gov.uk>
2023-11-14 11:12:15 +0000

    managed to print all issues in my jira board

--- ticket_creator.py
+++ ticket_creator.py
@@ -4,6 +4,12 @@ from jira import JIRA
 
 load_dotenv()
 jira_auth_key = os.getenv('JIRA_AUTH_KEY')
-email = os.getenv('JIRA_EMAIL')
+jira_email = os.getenv('JIRA_EMAIL')
 jira_server = os.getenv('JIRA_SERVER')
-jiraOptions = {'server': jira_server }
+jiraOptions = {'server': jira_server } #
+
+jira = JIRA(options=jiraOptions, basic_auth=(jira_email, jira_auth_key))
+
+
+for singleIssue in jira.search_issues(jql_str='project = TJIC'):
+    print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary,singleIssue.fields.reporter.displayName))
