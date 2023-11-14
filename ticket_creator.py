b7d0818c46313f3f0c354939bca6eda315f2f1bd
Hakan Bas <hakan.bas@digital.hmrc.gov.uk>
2023-11-14 11:37:45 +0000

    managed to create new issues in jira board

--- ticket_creator.py
+++ ticket_creator.py
@@ -9,16 +9,14 @@ jira_email = os.getenv('JIRA_EMAIL') #email used for Jira account
 jira_server = os.getenv('JIRA_SERVER') # Jira board URL, looks like ______.atlassian.net
 jiraOptions = {'server': jira_server } #
 
-jira = JIRA(options=jiraOptions, basic_auth=(jira_email, jira_auth_key))
-
-
-for singleIssue in jira.search_issues(jql_str='project = TJIC'):
-    print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary,singleIssue.fields.reporter.displayName))
-
-
+def connect_to_jira():
+    jira = JIRA(options=jiraOptions, basic_auth=(jira_email, jira_auth_key))
+    return jira
 
 def create_new_issue():
     new_issue = jira.create_issue(project='TJIC', summary='New issue from jira-python',
-                                description='Look into this one', issuetype={'name': 'Bug'})
-
-create_new_issue()
+                                description='Look into this one', issuetype={'id': '10001'})
+    
+def list_all_issues():
+    for singleIssue in jira.search_issues(jql_str='project = TJIC'):
+        print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary,singleIssue.fields.reporter.displayName))
